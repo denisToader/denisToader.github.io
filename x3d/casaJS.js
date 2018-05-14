bd8d6c2652a42a8b108ab4cf9151522e3bbdbb66
denisToader <toader.denis@gmail.com>
2018-05-14 23:45:31 +0300

    JIUBNCVXSDVAWIU

--- x3d/casaJS.js
+++ x3d/casaJS.js
@@ -1,5 +1,10 @@
 function forward(){
-	var speed = parseFloat(document.getElementById("viewpoint").getAttribute("fieldofview"))+0.1
+	var speed = parseFloat(document.getElementById("viewpoint").getAttribute("fieldofview"))-0.1;
+	document.getElementById("viewpoint").setAttribute("fieldofview", speed.toString()) ;
+}
+
+function backwards(){
+	var speed = parseFloat(document.getElementById("viewpoint").getAttribute("fieldofview"))+0.1;
 	document.getElementById("viewpoint").setAttribute("fieldofview", speed.toString()) ;
 }
 
@@ -8,6 +13,6 @@ window.addEventListener("deviceorientation", on_device_orientation)
 function on_device_orientation(e)
 {
 	document.getElementById("orientation_id").innerHTML = e.alpha.toFixed(1) + " " + e.beta.toFixed(1) + " " + e.gamma.toFixed(1);
-	var viewpoint = document.getElementById("id_viewpoint");
+	var viewpoint = document.getElementById("viewpoint");
 	viewpoint.setAttribute("orientation","0 1 0 " + e.alpha * Math.PI / 180);
 }

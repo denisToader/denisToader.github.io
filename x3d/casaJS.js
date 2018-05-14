d68d8f973a8dc22015ff641ecb4cb77d976aa5f3
denisToader <toader.denis@gmail.com>
2018-05-14 23:42:07 +0300

    lkjklj

--- x3d/casaJS.js
+++ x3d/casaJS.js
@@ -2,3 +2,12 @@ function forward(){
 	var speed = parseFloat(document.getElementById("viewpoint").getAttribute("fieldofview"))+0.1
 	document.getElementById("viewpoint").setAttribute("fieldofview", speed.toString()) ;
 }
+
+window.addEventListener("deviceorientation", on_device_orientation)
+
+function on_device_orientation(e)
+{
+	document.getElementById("orientation_id").innerHTML = e.alpha.toFixed(1) + " " + e.beta.toFixed(1) + " " + e.gamma.toFixed(1);
+	var viewpoint = document.getElementById("id_viewpoint");
+	viewpoint.setAttribute("orientation","0 1 0 " + e.alpha * Math.PI / 180);
+}

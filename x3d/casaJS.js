7eefc5ee3c0a3e3692b7a4344e90209ca244fd66
denisToader <toader.denis2308@gmail.com>
2018-05-15 12:12:28 +0300

    pos

--- x3d/casaJS.js
+++ x3d/casaJS.js
@@ -1,37 +1,11 @@
-function forward(){
-	//var speed = parseFloat(document.getElementById("viewpoint").getAttribute("fieldofview"))-0.1;
-	//document.getElementById("viewpoint").setAttribute("fieldofview", speed.toString()) ;
-	/*var pos = document.getElementById("viewpoint").getAttribute("position");
-	var axes = pos.split(',');
-	var z = parseFloat(axes[2])-0.2;
-	document.getElementById("viewpoint").setAttribute("position", axes[0] + "," + axes[1] + "," + z.toString()) ;*/
-	//zoom(-0.1); //zoom in
-	$("x3dom-x3dElement-canvas").click();
-}
-
-function backwards(){
-	//var speed = parseFloat(document.getElementById("viewpoint").getAttribute("fieldofview"))+0.1;
-	//document.getElementById("viewpoint").setAttribute("fieldofview", speed.toString()) ;	
-	var pos = document.getElementById("viewpoint").getAttribute("position");
-	var axes = pos.split(',');
-	var z = parseFloat(axes[2])+0.2;
-	document.getElementById("viewpoint").setAttribute("position", axes[0] + "," + axes[1] + "," + z.toString()) ;
-	//zoom(0.1); //zoom out
-}
-
 window.addEventListener("deviceorientation", on_device_orientation)
 
 function on_device_orientation(e)
 {
 	document.getElementById("orientation_id").innerHTML = e.alpha.toFixed(1) + " " + e.beta.toFixed(1) + " " + e.gamma.toFixed(1);
 	var viewpoint = document.getElementById("viewpoint");
+	var pos = viewpoint.getAttribute("orientation");
 	viewpoint.setAttribute("orientation","0 1 0 " + e.alpha * Math.PI / 180);
-}
-
-function load(){
-
-    var e = $.Event("keydown");
-    e.which = 87; // Character 'W'
-    $("x3dom-x3dElement-canvas").trigger(e);
 	
+	document.getElementById("viewpp").innerHTML = pos;
 }

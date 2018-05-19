511594ee42818ec154dfa9101e229094c2cc83f4
denisToader <toader.denis@gmail.com>
2018-05-19 19:30:42 +0300

    1235489

--- x3d/casaJS.js
+++ x3d/casaJS.js
@@ -2,7 +2,7 @@ window.addEventListener("deviceorientation", on_device_orientation)
 
 function on_device_orientation(e)
 {
-	document.getElementById("orientation_id").innerHTML = e.alpha.toFixed(1) + " " + e.beta.toFixed(1) + " " + e.gamma.toFixed(1);
+	document.getElementById("orientation_id").innerHTML = e.alpha.toFixed(1);/* + " " + e.beta.toFixed(1) + " " + e.gamma.toFixed(1);*/
 	var viewpoint = document.getElementById("viewpoint");
 	var pos = viewpoint.getAttribute("orientation");
 	viewpoint.setAttribute("orientation","0 1 0 " + e.alpha * Math.PI / 180);

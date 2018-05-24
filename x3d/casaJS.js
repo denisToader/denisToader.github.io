e50e187ca379977a2e086f25a48267ff99eec889
denisToader <toader.denis@gmail.com>
2018-05-24 22:54:16 +0300

    987

--- x3d/casaJS.js
+++ x3d/casaJS.js
@@ -6,17 +6,15 @@ function on_device_orientation(e)
 {
 	//document.getElementBy("orientation_id").innerHTML = e.alpha.toFixed(1);/* + " " + e.beta.toFixed(1) + " " + e.gamma.toFixed(1);*/
 	//var pos = viewpoint.getAttribute("orientation");
-	//var viewpoint = document.getElementsByClassName("viewpoint");
+	var viewpoint = document.getElementsByClassName("viewpoint");
 	
-	var viewpoint = document.getElementById(aidi);
-	viewpoint.setAttribute("orientation","0 1 0 " + e.alpha * Math.PI / 180);
 	
-	/*var i;
+	var i;
 	for (i = 0; i < viewpoint.length; i++) {
 		var bind= viewpoint[i].getAttribute("set_bind");
 		if(bind == "true" || bind == true)
 			viewpoint[i].setAttribute("orientation","0 1 0 " + e.alpha * Math.PI / 180);
-	}*/
+	}
 }
 
 function view(nume) {

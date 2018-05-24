8cb057bb14bf0c67f5b2bfd3e80c28c34eddc630
denisToader <toader.denis@gmail.com>
2018-05-24 22:33:10 +0300

    sffew

--- x3d/casaJS.js
+++ x3d/casaJS.js
@@ -3,9 +3,14 @@ window.addEventListener("deviceorientation", on_device_orientation)
 function on_device_orientation(e)
 {
 	//document.getElementBy("orientation_id").innerHTML = e.alpha.toFixed(1);/* + " " + e.beta.toFixed(1) + " " + e.gamma.toFixed(1);*/
-	var viewpoint = document.getElementByClass("viewpoint");
 	//var pos = viewpoint.getAttribute("orientation");
-	viewpoint.setAttribute("orientation","0 1 0 " + e.alpha * Math.PI / 180);
+	var viewpoint = document.getElementsByClass("viewpoint");
 	
+	
+	var i;
+	for (i = 0; i < viewpoint.length; i++) {
+		if(viewpoint[i].getAttribute("set_bind") == "true")
+			viewpoint.setAttribute("orientation","0 1 0 " + e.alpha * Math.PI / 180);
+	}
 }
 

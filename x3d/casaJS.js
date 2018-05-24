aed441b15bf42c691cb8160f4d07433ccd460bc4
denisToader <toader.denis@gmail.com>
2018-05-24 23:05:22 +0300

    1561

--- x3d/casaJS.js
+++ x3d/casaJS.js
@@ -6,7 +6,7 @@ function on_device_orientation(e)
 {
 	//document.getElementBy("orientation_id").innerHTML = e.alpha.toFixed(1);/* + " " + e.beta.toFixed(1) + " " + e.gamma.toFixed(1);*/
 	//var pos = viewpoint.getAttribute("orientation");
-	var viewpoint = document.getElementsByClassName("viewpoint");
+	/*var viewpoint = document.getElementsByClassName("viewpoint");
 	
 	
 	var i;
@@ -14,10 +14,19 @@ function on_device_orientation(e)
 		var bind= viewpoint[i].getAttribute("set_bind");
 		if(bind == "true" || bind == true)
 			viewpoint[i].setAttribute("orientation","0 1 0 " + e.alpha * Math.PI / 180);
+	}*/
+	var viewpoint = document.getElementsByClassName("front");
+	
+	switch(aidi) {
+		case "front": viewpoint.setAttribute("position","10,14,17"); break;
+		case "intrare": viewpoint.setAttribute("position","10,14,17"); break;
 	}
+	
+	viewpoint.setAttribute("orientation","0 1 0 " + e.alpha * Math.PI / 180);
+	
 }
 
 function view(nume) {
-	document.getElementById(nume).setAttribute('set_bind','true');
+	//document.getElementById(nume).setAttribute('set_bind','true');
 	aidi=nume;
 }
